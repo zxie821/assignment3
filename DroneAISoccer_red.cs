8014332b6a33faa6d50d1dabc6a7f9d1e7927934
xzp <xxxzp97@gmail.com>
2020-04-12 14:38:08 +0200

    init

---
+++ DroneAISoccer_red.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+
+//namespace UnityStandardAssets.Vehicles.Car
+//{
+[RequireComponent(typeof(DroneController))]
+public class DroneAISoccer_red : MonoBehaviour
+{
+    private DroneController m_Drone; // the drone controller we want to use
+
+    public GameObject terrain_manager_game_object;
+    TerrainManager terrain_manager;
+
+    public GameObject[] friends;
+    public string friend_tag;
+    public GameObject[] enemies;
+    public string enemy_tag;
+
+    public GameObject own_goal;
+    public GameObject other_goal;
+    public GameObject ball;
+
+
+    private void Start()
+    {
+        // get the car controller
+        m_Drone = GetComponent<DroneController>();
+        terrain_manager = terrain_manager_game_object.GetComponent<TerrainManager>();
+
+
+        // note that both arrays will have holes when objects are destroyed
+        // but for initial planning they should work
+        friend_tag = gameObject.tag;
+        if (friend_tag == "Blue")
+            enemy_tag = "Red";
+        else
+            enemy_tag = "Blue";
+
+        friends = GameObject.FindGameObjectsWithTag(friend_tag);
+        enemies = GameObject.FindGameObjectsWithTag(enemy_tag);
+
+        ball = GameObject.FindGameObjectWithTag("Ball");
+
+
+        // Plan your path here
+        // ...
+    }
+
+
+    private void FixedUpdate()
+    {
+
+
+        // Execute your path here
+        // ...
+
+        Vector3 avg_pos = Vector3.zero;
+
+        foreach (GameObject friend in friends)
+        {
+            avg_pos += friend.transform.position;
+        }
+        avg_pos = avg_pos / friends.Length;
+        //Vector3 direction = (avg_pos - transform.position).normalized;
+        Vector3 direction = (own_goal.transform.position - transform.position).normalized;
+
+
+
+        // this is how you access information about the terrain
+        int i = terrain_manager.myInfo.get_i_index(transform.position.x);
+        int j = terrain_manager.myInfo.get_j_index(transform.position.z);
+        float grid_center_x = terrain_manager.myInfo.get_x_pos(i);
+        float grid_center_z = terrain_manager.myInfo.get_z_pos(j);
+
+        Debug.DrawLine(transform.position, ball.transform.position, Color.black);
+        Debug.DrawLine(transform.position, own_goal.transform.position, Color.green);
+        Debug.DrawLine(transform.position, other_goal.transform.position, Color.yellow);
+        Debug.DrawLine(transform.position, friends[0].transform.position, Color.cyan);
+        Debug.DrawLine(transform.position, enemies[0].transform.position, Color.magenta);
+
+
+
+        // this is how you control the car
+        m_Drone.Move_vect(direction);
+        //m_Car.Move(0f, -1f, 1f, 0f);
+
+
+    }
+}
+//}
---
+++ DroneController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class DroneController : MonoBehaviour
+{
+
+    public Vector3 velocity;
+
+    public float max_speed = 15f;
+    public float max_acceleration = 15f;
+    public Vector3 desired_force;
+
+    private float v = 0f; //desired acceleration first component
+    private float h = 0f; //desired acceleration second component
+
+    private Rigidbody rb;
+
+    public void Move(float h_in, float v_in)
+    { 
+        h = h_in * 1000f * max_acceleration;
+        v = v_in * 1000f * max_acceleration;
+        desired_force = new Vector3(h, 0f, v);
+        if(desired_force.magnitude > 1000f * max_acceleration)
+        {
+            desired_force = desired_force.normalized * 1000f * max_acceleration;
+        }
+    }
+
+    public void Move_vect(Vector3 desired_relative_force)
+    {
+        if(desired_relative_force.magnitude > 1f)
+        {
+            desired_force = desired_relative_force.normalized * 1000f * max_acceleration;
+        }
+        else
+        {
+            desired_force = desired_relative_force * 1000f * max_acceleration;
+        }
+        desired_relative_force.y = 0f; //removing vertical component
+            
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        velocity = rb.velocity; // for debugging purposes
+        rb.AddForce(desired_force);
+
+        //Debug.Log(velocity.magnitude);
+    }
+
+    
+
+    
+}
